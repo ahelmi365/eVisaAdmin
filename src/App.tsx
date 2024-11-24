2167e1814ae2a7a35485e4e256e6b5840057ec13
Ali Helmi <ahelmi365@gmail.com>
2024-11-24 13:15:16 +0200

    use navbar and footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import "./App.css";
+import Footer from "./components/Footer/Footer";
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
   return (
-    <>
-      <button className="btn btn-primary">Hello</button>
-    </>
+    <div className="container app-container">
+      <div className="top">
+        <NavBar />
+      </div>
+      <div className="main">
+        <button className="btn btn-primary">Hello</button>
+      </div>
+      <div className="footer"><Footer/></div>
+    </div>
   );
 }
 
