1cf5daedac8e147eb0cd5d7a1c07708012e00e4a
Ali Helmi <ahelmi365@gmail.com>
2024-11-24 16:49:54 +0200

    init view all requests

--- src/components/ViewAllRequests/ViewAllRequests.tsx
+++ src/components/ViewAllRequests/ViewAllRequests.tsx
@@ -1,9 +1,48 @@
-import React from 'react'
-
 const ViewAllRequests = () => {
-  return (
-    <div>ViewAllRequests</div>
-  )
-}
+  const requests = [
+    {
+      first: "Ali",
+      last: "Helmy",
+      handle: "@ali",
+    },
+    {
+      first: "Taha",
+      last: "Helmy",
+      handle: "@taha",
+    },
+    {
+      first: "Mohamed",
+      last: "Helmy",
+      handle: "@mo",
+    },
+  ];
+  const renderedRequestsRows = requests.map((request, index) => (
+    <tr key={request.handle}>
+      <th scope="row">{index+1}</th>
+      <td>{request.first}</td>
+      <td>{request.last}</td>
+      <td>{request.handle}</td>
+      <td><button className="btn btn-primary">View</button></td>
+    </tr>
+  ));
 
-export default ViewAllRequests
+  return (
+    <div>
+      <h4 className="mb-4">All Requests</h4>
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+            <th scope="col">Handle</th>
+            <th scope="col">View</th>
+          </tr>
+        </thead>
+        <tbody>{renderedRequestsRows}</tbody>
+      </table>
+    </div>
+  );
+};
+
+export default ViewAllRequests;
