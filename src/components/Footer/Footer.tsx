2381051eecebcb154ce21ff140571719837ac36a
Ali Helmi <ahelmi365@gmail.com>
2024-11-24 16:48:45 +0200

    set font size

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,10 +2,13 @@ import getLogo from "../../assets/images/get_logo.png";
 
 const Footer = () => {
   return (
-    <footer className="mb-4">
+    <footer className="mb-2">
       <div className="al-footer-right">
         <img src={getLogo} style={{ height: "30px", width: "25px" }} />{" "}
+        <span style={{fontSize:"0.9rem"}}>
+
         Copyright © 2024 GET Group. All Rights Reserved.
+        </span>
       </div>
       <div className="al-footer-main clearfix">
         <div className="al-copy"></div>
