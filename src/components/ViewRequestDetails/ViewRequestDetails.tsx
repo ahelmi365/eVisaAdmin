95f135466d98ebe504987b9647f85273f58a860d
Ali Helmi <ahelmi365@gmail.com>
2024-11-26 16:48:07 +0200

    use customhook

--- src/components/ViewRequestDetails/ViewRequestDetails.tsx
+++ src/components/ViewRequestDetails/ViewRequestDetails.tsx
@@ -5,28 +5,9 @@ import passportPhoto from "../../assets/images/passportPhoto.jpg";
 import visaInvitationLetterPDF from "../../assets/pdf/invitationLetter.pdf";
 import ImagePreview from "../ImagePreview/ImagePreview";
 import ViewPDFFile from "../viewPDFFile/ViewPDFFile";
+import useViewRequestDetails from "./useViewRequestDetails";
 const ViewRequestDetails = () => {
-  const applicantInfo = {
-    fullname: "Sarah Martin",
-    applicationNumber: "#Ref000012",
-    visaType: "Business",
-    entriesNumber: "Single",
-    durationOfStay: "14",
-    haveapreviousvisa: "No",
-    perviousVisaNumber: "---",
-    purposeOfVisit: "Attend Meeting",
-    passportNumber: "P123456AA",
-    fullName: "Sarah Martin",
-    currentAddress: " Ottawa, Canda",
-    gender: "Female",
-    dateOfBirth: "08/01/1990",
-    arrivalDate: "12/01/2024",
-    departureDate: "12/14/2024",
-    mobileNumber: "+1002345678",
-    email: "sarah_martin@domain.com",
-    emergencyContactName: "Martin Scorsese",
-    emergencyContactNumber: "+10023443200",
-  };
+  const applicantInfo = useViewRequestDetails();
   return (
     <div className="view-request-details-container">
       <h4 className="text-center">View Request Details</h4>
